826b6215f9e42a396a920802852750e8d883112f
Jake Klassen <jklassendev@gmail.com>
2019-01-18 09:33:17 -0500

    component tweak

--- src/components/position.ts
+++ src/components/position.ts
@@ -1,9 +1,3 @@
 export class Position {
-  public x = 0;
-  public y = 0;
-
-  constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
-  }
+  constructor(public x: number, public y: number) {}
 }
