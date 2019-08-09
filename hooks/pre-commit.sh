9ab9082f0c703f58f5217dda8d23819cdbaed703
Jake Klassen <jklassendev@gmail.com>
2019-08-09 16:15:51 -0400

    Update hook script to compare to master only

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -2,7 +2,7 @@
 
 # If we have modified src/ files that do not end in `.test.ts`,
 # rebuild the docs and add a commit.
-git diff --name-only master..HEAD \
+git diff --name-only master \
   | grep -Ei 'src/.*.ts' \
   | if grep -Eivq 'src/.*.test.ts'; then \
       yarn docs; \
