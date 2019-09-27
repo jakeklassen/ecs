7dfb58c7d8000589eb41a14b3c428feb95836ae6
Jake Klassen <jklassendev@gmail.com>
2019-09-26 20:58:27 -0400

    event emitter class

--- src/event-emitter.ts
+++ src/event-emitter.ts
@@ -1,14 +1,39 @@
+export type Handler = (...args: any[]) => void;
+
 /**
  * @example
- * export const CollisionEventKey = Symbol.for('collision');
+ * const CollisionEventKey = Symbol.for('collision');
  * type CollisionEventHandler = (a: Entity, b: Entity) => void;
  *
- * const ee = new EventEmitter();
- * ee.on<CollisionEventHandler>(CollisionEventKey, (a: Entity, b: Entity) => {});
+ * const emitter = new EventEmitter();
+ * emitter.on<CollisionEventHandler>(CollisionEventKey, (a: Entity, b: Entity) => {
+ *   // do stuff
+ * });
  */
 export class EventEmitter {
-  public on<T extends (...args: any[]) => void>(key: symbol, handler: T) {}
-}
+  private listeners = new Map<symbol, Set<Handler>>();
 
-// Imagine `world` being the real event emitter in the end.
-// world.events.on(...)
+  public on<T extends Handler>(key: symbol, handler: T) {
+    if (this.listeners.has(key)) {
+      this.listeners.get(key)!.add(handler);
+    } else {
+      this.listeners.set(key, new Set([handler]));
+    }
+  }
+
+  public emit(key: symbol, ...args: any[]) {
+    if (this.listeners.has(key) === false) {
+      return;
+    }
+
+    this.listeners.get(key)!.forEach(handler => handler(...args));
+  }
+
+  public off(key: symbol, handler: Handler) {
+    if (this.listeners.has(key) === false) {
+      return false;
+    }
+
+    return this.listeners.get(key)!.delete(handler);
+  }
+}
